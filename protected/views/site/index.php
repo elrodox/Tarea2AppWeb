9792324380cc67ec42deaff8c8a55e1e526f82f7
Rodolfo Guíñez <elrodox@gmail.com>
2015-05-15 17:27:12 -0300

    Se crea interfaz de pantallas

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,18 +3,26 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div class="ui grid">
+    <div class="five wide column">
+        <div class="ui piled segment">
+            <a href="?r=site/basica">
+                <h2 class="ui header">Consulta Basica</h2>
+            </a>
+        </div>
+    </div>
+    <div class="five wide column">
+        <div class="ui piled segment">
+            <a href="?r=site/media">
+                <h2 class="ui header">Consulta Media</h2>
+            </a>
+        </div>
+    </div>
+    <div class="five wide column">
+        <div class="ui piled segment">
+            <a href="?r=site/avanzada">
+                <h2 class="ui header">Consulta Avanzada</h2>
+            </a>
+        </div>
+    </div>
+</div>
---
+++ protected/views/site/media.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this SiteController */
+
+?>
+
+<br/><div class="ui inverted blue large menu">
+    <a href="index.php" class="item">
+        <i class="home icon"></i> Inicio
+    </a>
+    <a href="?r=site/basica" class="item">
+        <i class="mail icon"></i> Consulta Simple
+    </a>
+    <a href="?r=site/media" class="active item">
+        <i class="mail icon"></i> Consulta Media
+    </a>
+    <a href="?r=site/avanzada" class="item">
+        <i class="mail icon"></i> Consulta Avanzada
+    </a>
+</div>
+
+consultita...
