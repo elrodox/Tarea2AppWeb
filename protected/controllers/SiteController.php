827407cec1006ae4b25df850b9393d2cfca3a490
Rodolfo Guíñez <elrodox@gmail.com>
2015-05-28 16:34:59 -0300

    Se crea la tercera consulta, la cual involucra 5 tablas

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -44,9 +44,36 @@ class SiteController extends Controller
             'emps' => $emps
         ));
     }
+
+
+//    datos de empleados de depto de Marketing
+//    que tengan un salario superior a 70.000
+//    y que la fecha from_date de su titulo sea posterior a 1990-01-01
     public function actionAvanzada()
     {
-        $this->render('avanzada');
+        $con = Yii::app()->db;
+        $emps = $con->createCommand("
+    		select * from salaries as s
+            inner join
+            (select e.emp_no, e.birth_date, e.first_name, e.last_name, e.gender, e.hire_date
+            from employees as e inner join dept_emp as de
+            on e.emp_no=de.emp_no
+            where (de.from_date between '1990-01-01' and '2000-01-01')
+            and de.dept_no in (
+                select d.dept_no from departments as d, dept_manager as dm
+                where d.dept_no = dm.dept_no
+                and d.dept_name = 'Marketing'
+                and de.from_date between '1995-01-01' and '2000-01-01')
+            and e.first_name like '%Ro%') as a
+            on a.emp_no = s.emp_no
+            where s.salary < 1000000
+            and s.salary > 100000
+            and s.from_date between '1990-01-01' and '2000-01-01'
+            order by s.salary asc
+    		")->queryAll();
+        $this->render('basica', array(
+            'emps' => $emps
+        ));
     }
 
 
